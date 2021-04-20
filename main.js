2112700ac4821485bc8440a4c50b02ff3877fd96
wilckdj <djoseph.wilcken@gmail.com>
2021-04-20 17:54:44 -0600

    Update

--- main.js
+++ main.js
@@ -2,11 +2,7 @@ const links = [
     {
         label:"Week1 notes",
         url: "week1/index.html"
-    },
-/*    {
-        label:"Week2 notes",
-        url: "week2/index.html"
-    }*/
+    }
 ]
 
     var toc = document.getElementById("content")
