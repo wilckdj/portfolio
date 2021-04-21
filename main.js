a1f078b372d0aa23f0a21a09e87a8d963a37c3cd
wilckdj <djoseph.wilcken@gmail.com>
2021-04-20 18:14:34 -0600

    Update

--- main.js
+++ main.js
@@ -7,12 +7,10 @@ const links = [
 
 
     var toc = document.getElementById("tableOfContents")
-    console.log(toc)
     for (let i = 0; i<links.length; i++) {
         console.log (i);
         var item = document.createElement("li");
         var a = document.createElement("a");
-        console.log(toc)
         //item.appendChild(document.createTextNode(links[i].label));
         a.textContent = links[i].label;
         a.setAttribute('href', links[i].url);
