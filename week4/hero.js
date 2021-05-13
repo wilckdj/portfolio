8404d97e6e918fbd7a648fd09d61b855fe9c7030
wilckdj <djoseph.wilcken@gmail.com>
2021-05-12 22:02:43 -0600

    week4

---
+++ week4/hero.js
@@ -0,0 +1,54 @@
+const form = document.forms['hero'];
+form.addEventListener('submit', makeHero, false);
+
+function makeHero(event) {
+    event.preventDefault(); // prevent the form from being submitted
+    const hero = {}; // create an empty object
+    hero.name = form.heroName.value; // create a name property based on the input field's value
+    hero.realName = form.realName.value;
+    hero.powers = [];
+    for (let i = 0; i < form.powers.length; i++) {
+        if (form.powers[i].checked) {
+            hero.powers.push(form.powers[i].value);
+        }
+    }
+    hero.category = form.category.value;
+    hero.age = form.age.value;
+    hero.city = form.city.value;
+    hero.origin = form.origin.value;
+    alert(JSON.stringify(hero)); // convert object to JSON string and display in alert dialog
+    return hero;
+}
+//document.forms.hero.powers[0].checked = true;
+
+/*form.addEventListener('submit',validate,false);
+function validate(event) {
+    const firstLetter = form.heroName.value[0];
+    if (firstLetter.toUpperCase() === 'X') {
+        event.preventDefault();
+        alert('Your name is not allowed to start with X!');
+    }
+}*/
+form.heroName.addEventListener('blur',validateInline, false);
+const label = form.querySelector('label');
+const error = document.createElement('div');
+error.classList.add('error');
+error.textContent = '! Your name is not allowed to start with X.';
+label.append(error);
+function validateInline() {
+    const heroName = this.value.toUpperCase();
+    if(heroName.startsWith('X')){
+    error.style.display = 'block';
+    } else {
+    error.style.display = 'none';
+    }
+}
+
+function disableSubmit(event) {
+    if(event.target.value === ''){
+        document.getElementById('submit').disabled = true;
+    } else {
+        document.getElementById('submit').disabled = false;
+    }
+}
+form.heroName.addEventListener('keyup',disableSubmit,false);
---
+++ week4/search.js
@@ -0,0 +1,28 @@
+const form = document.forms['search'];
+//const [input,button] = form.elements;
+
+const input = form.elements.searchInput;
+input.addEventListener('focus', () => console.log('focused'), false);
+input.addEventListener('blur', () => console.log('blurred'), false);
+input.addEventListener('change', () => console.log('changed'), false);
+
+form.addEventListener ('submit', search, false);
+/*function search(event) {
+    alert('Form Submitted');
+    event.preventDefault();
+}*/
+function search(event) {
+    alert(`You Searched for: ${input.value}`);
+    event.preventDefault();
+}
+input.value = 'Search Here';
+
+input.addEventListener('focus', function(){
+    if (input.value==='Search Here') {
+        input.value = '' 
+    }
+}, false);
+input.addEventListener('blur', function(){
+    if(input.value === '') {
+        input.value = 'Search Here';
+    } }, false);
