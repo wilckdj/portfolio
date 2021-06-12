7c5599fae8cd7c8359d7c4f0095dc6290b5e1f3f
wilckdj <djoseph.wilcken@gmail.com>
2021-06-12 15:35:09 -0600

    week8

--- main.js
+++ main.js
@@ -1,35 +1,49 @@
 const links = [{
         label: "Week 1 Notes",
-        url: "week1/index.html"
+        url: "week1/index.html",
+        block: 1
     },
     {
         label: "Week 2 Notes",
-        url: "week2/index.html"
+        url: "week2/index.html",
+        block: 1
     },
     {
         label: "Week 3 Notes",
-        url: "week3/index.html"
+        url: "week3/index.html",
+        block: 1
     },
     {
         label: "Week 4 Notes",
-        url: "week4/index.html"
+        url: "week4/index.html",
+        block: 1
     },
     {
         label: "Week 5 Notes",
-        url: "week5/index.html"
+        url: "week5/index.html",
+        block: 1
     },
     {
         label: "Week 6 Midterm Project",
-        url: "week6/index.html"
+        url: "week6/index.html",
+        block: 1
     },
     {
         label: "Week 7 Notes",
-        url: "week7/index.html"
+        url: "week7/index.html",
+        block: 2
+    },
+    {
+        label: "Week 8 Notes",
+        url: "week8/index.html",
+        block: 2
     }
 ]
 
 
-var toc = document.getElementById("tableOfContents")
+const block1 = document.getElementById("block1")
+const block2 = document.getElementById("block2")
+
 for (let i = 0; i < links.length; i++) {
     //console.log(i);
     var item = document.createElement("li");
@@ -38,7 +52,12 @@ for (let i = 0; i < links.length; i++) {
     a.textContent = links[i].label;
     a.setAttribute('href', links[i].url);
     item.appendChild(a);
-    toc.append(item);
+    if (links[i].block == 1) {
+        block1.append(item);
+    } else {
+        block2.append(item);
+    }
+    
 
 }
 
---
+++ week8/script.js
@@ -0,0 +1,44 @@
+const list = document.getElementById("swData")
+const prevB = document.getElementById("prev")
+const nextB = document.getElementById("next")
+let pageURLnext = "";
+let pageURLprev = "";
+let pagelist = 0;
+let navi = document.getElementById("navigate");
+
+fetch("https://swapi.dev/api/starships/")
+    .then(response => response.json())
+    .then((data) => {
+        pagelist = (Math.ceil(data.count / 10))
+        for (let i = 1; i <= pagelist; i++) {
+            let num = document.createElement("input")
+            num.setAttribute("type", "button");
+            num.setAttribute("value", i);
+            navi.appendChild(num)
+            num.addEventListener('click', () => fetchData(`https://swapi.dev/api/starships/?page=${i}`), false)
+        }
+    });
+
+fetchData("https://swapi.dev/api/starships/")
+
+function fetchData(fetchURL) {
+
+    if (fetchURL !== null) {
+        list.innerHTML = "";
+        fetch(fetchURL)
+            .then(response => response.json())
+            .then((data) => {
+                let obj = Object.entries(data.results);
+                pageURLnext = data.next;
+                pageURLprev = data.previous;
+                obj.forEach(item => {
+                    let lItem = document.createElement("li");
+                    lItem.textContent = item[1].name;
+                    list.appendChild(lItem);
+                })
+            })
+    } 
+}
+prevB.addEventListener('click', () => fetchData(pageURLprev), false);
+nextB.addEventListener('click', () => fetchData(pageURLnext), false);
+
